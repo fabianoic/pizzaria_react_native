6bbaf84840d21c9f00312805ec27c8d618dcab7b
Fabiano Ilha Campos <fabiano.fic@gmail.com>
2019-11-29 22:03:02 -0300

    adicionando buttomtabnavigation

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,32 @@
 import {createAppContainer} from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack';
+import {createBottomTabNavigator} from 'react-navigation-tabs';
 
 import Filter from './pages/Filter';
 import New from './pages/New';
 import ProductFilter from './pages/ProductFilter';
+
+const PedidoTabs = createBottomTabNavigator({
+  Itens: {
+    screen: ProductFilter,
+    navigationOptions: {
+      tabBarLabel: 'Itens',
+    },
+  },
+  Info: {
+    screen: New,
+    navigationOptions: {
+      tabBarLabel: 'Info',
+    },
+  },
+});
 
 export default createAppContainer(
   createStackNavigator(
     {
       Filter,
       New: {
-        screen: New,
+        screen: PedidoTabs,
       },
       ProductFilter,
     },
